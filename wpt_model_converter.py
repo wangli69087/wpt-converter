a88efd43fa47d247bc61c9844cc6ee2633d195b3
li1x.wang <li1x.wang@intel.com>
2021-09-17 15:29:32 +0800

    Update the format processing section

--- wpt_model_converter.py
+++ wpt_model_converter.py
@@ -16,7 +16,7 @@ def wpt_model_converter(input_path, output_path):
             target = os.path.join(output_path, js_name.split(os.path.sep)[-1].split("_")[0] + "_" + js_name.split(os.path.sep)[-1].split("_")[1].split(".")[0] + ".html")
         else:
             target = os.path.join(output_path, js_name.split(os.path.sep)[-1].split(".")[0] + ".html")
-        test_data_dir = js_name.split(os.path.sep)[-1].split(".")[0]
+        test_data_dir = js_name.split(os.path.sep)[-1].split(".js")[0]
         title_text = test_data_dir.replace("_"," ")
         html_head_content = """<!doctype html>
 <meta charset=utf-8>
@@ -43,7 +43,7 @@ def wpt_model_converter(input_path, output_path):
             html_writer.writelines(html_head_content) 
             with open(js_name, 'r') as js_reader:
                 js_content_list = js_reader.readlines()
-                global case_tag, before_tag, after_tag, case_name, case_condition1, case_condition2, case_condition3, total_ignored_line, after_total_ignored_line, current_line_index 
+                global case_tag, before_tag, after_tag, case_name, case_condition1, case_condition2, case_condition3, total_ignored_line, after_total_ignored_line, before_line_index, after_line_index, current_line_index 
                 case_tag = False
                 before_tag = False
                 after_tag = False
@@ -51,11 +51,18 @@ def wpt_model_converter(input_path, output_path):
                 case_condition1 = ""
                 case_condition2 = ""
                 case_condition3 = ""
-                total_ignored_line = 6
+                total_ignored_line = 4
                 after_total_ignored_line = 15
-                before_line_index = 0
-                after_line_index = 0
+                before_line_index = None
+                after_line_index = None
+                if "  before(async () => {\n" in js_content_list:
+                    before_line_index = js_content_list.index("  before(async () => {\n")
+                if "  after(async () => {\n" in js_content_list:
+                    after_line_index = js_content_list.index("  after(async () => {\n")
+                elif "  after(() => {\n" in js_content_list:
+                    after_line_index = js_content_list.index("  after(() => {\n")
                 for js_line in js_content_list:
+                    current_line_index = js_content_list.index(js_line)
                     if "import" in js_line:
                         continue
                     elif js_line.startswith("'use strict';"):
@@ -80,6 +87,16 @@ def wpt_model_converter(input_path, output_path):
                         continue
                     elif js_line.startswith("  let beforeNumTensors;"):
                         continue
+                    elif js_line.startswith("  before(async () => {\n"):
+                        before_tag = True
+                        continue
+                    elif js_line.startswith("    const context = navigator.ml.createContext();"):
+                        before_tag = False
+                        continue
+                    elif js_line.startswith("    const builder = new MLGraphBuilder(context);"):
+                        continue
+                    elif before_tag:
+                        continue
                     elif js_line.startswith("    graph = await build"):
                         case_condition1 =js_line
                         continue
@@ -89,11 +106,18 @@ def wpt_model_converter(input_path, output_path):
                     elif js_line.startswith("    fusedGraph = await build"):
                         case_condition3 =js_line
                         continue
+                    elif js_line.startswith("  after(() => {") or js_line.startswith("  after(async () => {"):
+                        after_tag = True
+                        continue
+                    elif (js_line.startswith("  async function test")) and ("(graph, inputFile, expectedFile" in js_line):
+                        after_tag = False
+                    elif after_tag:
+                        continue
                     elif js_line.startswith("  it(") or js_line.startswith("  it.skip("):
                         case_tag = True
                         new_js_line = "  promise_test(async () => {  \n"
                         if "(fused ops)" in js_line:
-                            case_name = js_line.split(",")[0].split("(")[1] + ' (' + js_line.split(",")[0].split("(")[2]
+                            case_name = js_line.split(",")[0].split("(")[1] + '(' + js_line.split(",")[0].split("(")[2]
                             html_writer.writelines(new_js_line)
                             html_writer.writelines(case_condition2)
                             html_writer.writelines(case_condition3)
@@ -104,52 +128,21 @@ def wpt_model_converter(input_path, output_path):
                         continue
                     elif js_line.startswith("  });"):
                         if case_tag:
-                            js_line = js_line.replace("  });\n", "  }, " + case_name + ");\n")
+                            js_line = js_line.replace("  });\n", "  }, " + '"test ' + title_text + " / " + case_name.strip("'") + '"' + ");\n")
                         else:
                             continue
                     elif js_line.startswith("});"):
                         continue
-
-                    current_line_index = js_content_list.index(js_line)
-                    if js_content_list.count(js_line) == 2:
-                        print(js_line)
-                    #     del js_content_list[js_line]
-                    #     current_line_index = js_content_list.index(js_line, )
-                    if js_line.startswith("  before(async () => {"):
-                        before_tag = True
-                        before_line_index = js_content_list.index(js_line)
-                        continue
-
-                    elif current_line_index >= before_line_index:
-                        continue
-
-                    if js_line.startswith("  after(() => {") or js_line.startswith("  after(async () => {"):
-                        after_tag = True
-                        after_line_index = js_content_list.index(js_line)
-                        continue
-                    if after_tag:
-                        if js_line.startswith("    if (typeof _tfengine !== 'undefined') {"):
-                            current_line_index = js_content_list.index(js_line, 20)
-                        elif js_line.startswith("    }"):
-                            current_line_index = js_content_list.index(js_line, 90)
+                    if (current_line_index > before_line_index) and (current_line_index < after_line_index): 
+                        html_writer.writelines(js_line[2:])
                     else:
-                        current_line_index = js_content_list.index(js_line)
-
-                    if  (total_ignored_line + before_line_index)  >= current_line_index > before_line_index:
-                        if before_tag:
-                            continue
-                    if  (after_total_ignored_line + after_line_index)  >= current_line_index > after_line_index:
-                        if after_tag:
-                           continue
-
-
-                    html_writer.writelines(js_line)
+                        html_writer.writelines(js_line)
             html_writer.writelines('</script>\n') 
 
 
 if __name__ == "__main__": 
     # input_path = sys.argv[1]
     # output_path = sys.argv[2]
-    input_path = "./models_js"
+    input_path = "./models"
     output_path = "./models_html2"
     wpt_model_converter(input_path, output_path)
